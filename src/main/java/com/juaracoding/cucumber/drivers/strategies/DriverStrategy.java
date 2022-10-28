a5f77019c05fa3b0851e7514073ad3642fd7aa03
Putri Pratiwi <putripratiwis@gmail.com>
2022-10-28 20:49:04 +0700

    First Commit

---
+++ src/main/java/com/juaracoding/cucumber/drivers/strategies/DriverStrategy.java
@@ -0,0 +1,8 @@
+package com.juaracoding.cucumber.drivers.strategies;
+
+import org.openqa.selenium.WebDriver;
+
+public interface DriverStrategy {
+    WebDriver setStrategy();
+}
+
---
+++ src/main/java/com/juaracoding/cucumber/drivers/strategies/DriverStrategyImplementer.java
@@ -0,0 +1,23 @@
+package com.juaracoding.cucumber.drivers.strategies;
+
+import com.juaracoding.cucumber.utils.Constants;
+
+public class DriverStrategyImplementer {
+
+    public static DriverStrategy chooseStrategy(String strategy) {
+        switch (strategy) {
+            case Constants.CHROME:
+                return new Chrome();
+
+            case Constants.CHROMEHEADLESS:
+                return new ChromeHeadless();
+
+            case Constants.FIREFOX:
+                return new Firefox();
+
+            default:
+                return null;
+        }
+    }
+
+}
